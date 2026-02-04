27d3fe835568cfdc4f3517bc9c7dde4b62663b72
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2026-02-04 10:51:37 +0700

    up

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -88,4 +88,4 @@ app.delete('/books/:id', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port http://localhost:${PORT}`));
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}...`));
