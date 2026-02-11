e25eb5bbc98776f9bbf5e62aaf451f4482efa16a
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2026-02-11 10:43:12 +0700

    up

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -11,17 +11,15 @@ const app = express();
 //const base_url = "https://api.example.com";
 const base_url = "http://localhost:3000";
 
-// Set the template engine
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/public/views'));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/", async (req, res) => {
     try {
